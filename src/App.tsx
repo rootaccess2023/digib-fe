b942670ebfb7d9c1cc21b92a46fe312329f737ae
redroboto <vince.pleyto@gmail.com>
2025-06-27 19:57:48 +0800

    feat: Added AppRoutes component to contain the different routes. Initialized Navbar to contain some placeholder links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router } from 'react-router'
+import Navbar from './components/Navbar'
+import AppRoutes from './components/AppRoutes'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-blue-600 text-5xl text-center">This is the Digi-B page</h1>
+      <Router>
+        <Navbar/>
+        <AppRoutes />
+      </Router>
     </>
   )
 }
---
+++ src/components/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import SignupForm from "../features/SignupForm";
+import Welcome from "../features/Welcome";
+import LoginForm from "../features/LoginForm";
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome/>}/>
+            <Route path="/signup" element={<SignupForm/>}/>
+            <Route path="/login" element={<LoginForm/>}/>
+        </Routes>
+    )
+}
+
+export default AppRoutes;
