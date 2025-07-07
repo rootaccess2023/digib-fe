4a6767050ff1b35a6c470b764d5deaadad95de8e
redroboto <vince.pleyto@gmail.com>
2025-07-07 22:29:43 +0800

    chore:removed unused code which was commented out earlier

--- src/features/LogoutButton.tsx
+++ src/features/LogoutButton.tsx
@@ -1,20 +1,17 @@
 import { useNavigate } from "react-router"
 import { logout } from "../api/auth";
-// import { useUser } from "../contexts/AuthProviderContext";
 import { useToken } from "../contexts/AuthProviderContext";
-
 
 const LogoutButton = () => {
     const navigate = useNavigate();
-    // const {user} = useUser();
     const {token, setToken} = useToken();
 
     const handleLogout = async () => {
         try {
             await logout(token?.token);
             console.log("Log out successful.")
-        } catch (error) {
-            console.error("Failed to log out: ", error)
+        } catch (err : any) {
+            console.error("Failed to log out: ", err.message);
         }
         setToken(null);
         navigate("/");
