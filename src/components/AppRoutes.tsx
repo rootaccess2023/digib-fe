ac3ca361c7250c6c7fd0afa43583e16ebbebc565
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2025-07-15 23:33:14 +0200

    style: update dashboard styles

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,6 +4,8 @@ import Welcome from "../features/Welcome";
 import LoginForm from "../features/LoginForm";
 import Dashboard from "../pages/Dashboard";
 import Profile from "../pages/Profile";
+import RequestDocument from "../pages/RequestDocument";
+import MyRequests from "../pages/MyRequests";
 
 function AppRoutes() {
   return (
@@ -13,6 +15,8 @@ function AppRoutes() {
       <Route path="/login" element={<LoginForm />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/request" element={<RequestDocument />} />
+      <Route path="/requests" element={<MyRequests />} />
     </Routes>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
 import useAuth from "../contexts/useAuth";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function Dashboard() {
   // imported user and token just to test if login works
@@ -15,9 +16,52 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Welcome to your dashboard!</h1>
-      <p className="mt-4">You are now logged in.</p>
+    <div className="h-full ui-bg --primary mx-auto p-8 bg-white">
+      <div className="border-b border-gray-200 pb-4 mb-6">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
+
+      <section className="space-y-6">
+        <div className="bg-white p-4 rounded">
+          <h2 className="text-sm font-medium text-gray-700 mb-3 uppercase tracking-wide">
+            Overview
+          </h2>
+          <div className="text-lg text-gray-700 mb-2">
+            Welcome to your Barangay's digital hub.
+          </div>
+          <div className="text-base text-gray-600">
+            You are now logged in. Here you can request documents, track your
+            requests, and manage your account—all online.
+          </div>
+        </div>
+
+        <ul className="flex justify-between gap-8">
+          <li className="w-full flex items-center h-18 bg-white rounded text-2xl font-bold shadow group cursor-pointer">
+            <span className="flex flex-1 items-center px-6 h-full group-hover:bg-[var(--olive-color)] rounded-tl rounded-bl">
+              Request Document
+            </span>
+            <span className="h-full flex items-center bg-white px-4 rounded-tr rounded-br">
+              <FaArrowRight className="size-4" />
+            </span>
+          </li>
+          <li className="w-full flex items-center h-18 bg-white rounded text-2xl font-bold shadow group cursor-pointer">
+            <span className="flex flex-1 items-center px-6 h-full group-hover:bg-[var(--olive-color)] rounded-tl rounded-bl">
+              Request Document
+            </span>
+            <span className="h-full flex items-center bg-white px-4 rounded-tr rounded-br">
+              <FaArrowRight className="size-4" />
+            </span>
+          </li>
+          <li className="w-full flex items-center h-18 bg-white rounded text-2xl font-bold shadow group cursor-pointer">
+            <span className="flex flex-1 items-center px-6 h-full group-hover:bg-[var(--olive-color)] rounded-tl rounded-bl">
+              Request Document
+            </span>
+            <span className="h-full flex items-center bg-white px-4 rounded-tr rounded-br">
+              <FaArrowRight className="size-4" />
+            </span>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
